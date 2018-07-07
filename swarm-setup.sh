a1e4c2d8f5fdde6e388ffae2865329d04bb295c8
stormcat24 <stormcat24@stormcat.io>
2018-07-08 00:30:16 +0900

    add swarm init script

---
+++ swarm-setup.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env sh
+
+if [ ! `which docker` ]; then
+  echo "docker is not installed." 1>&2
+  exit 1
+fi
+
+docker exec -it manager docker swarm init
+if [ $? -ne 0 ]; then
+  echo "docker swarm init failed." 1>&2
+  exit 1
+fi
+
+JOIN_TOKEN=$(docker exec -it manager docker swarm join-token manager --quiet | tr '\r' ' ' | tr '\n' ' ')
+if [ $? -ne 0 ]; then
+  echo "get swarm join token failed." 1>&2
+  exit 1
+fi
+
+docker exec -it worker01 docker swarm join --token $JOIN_TOKEN manager:2377
+if [ $? -ne 0 ]; then
+  echo "worker01: docker swarm join failed." 1>&2
+  exit 1
+fi
+
+docker exec -it worker02 docker swarm join --token $JOIN_TOKEN manager:2377
+if [ $? -ne 0 ]; then
+  echo "worker02: docker swarm join failed." 1>&2
+  exit 1
+fi
+
+docker exec -it worker03 docker swarm join --token $JOIN_TOKEN manager:2377
+if [ $? -ne 0 ]; then
+  echo "worker03: docker swarm join failed." 1>&2
+  exit 1
+fi
